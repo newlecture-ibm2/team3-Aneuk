28f1e8a3f5f005c2af3fc9f374898ceff73f8982
heny <hyeunjjj@gmail.com>
2026-04-24 13:14:58 +0900

    feat:백엔드 개발환경 구축중

---
+++ backend/src/main/java/com/anook/backend/AnookBackendApplication.java
@@ -0,0 +1,11 @@
+package com.anook.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnookBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AnookBackendApplication.class, args);
+    }
+}
